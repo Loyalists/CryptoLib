50760d05f9060fbd4241181012d186d1b602edcd
Loyalists <13900536+Loyalists@users.noreply.github.com>
2023-05-17 17:26:27 +0800

    Create TDESKeyComponent.cs

---
+++ CryptoLib/CryptoLib/Algorithm/Key/TDESKeyComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLib.Algorithm.Key
+{
+    public enum TDESKeyComponent
+    {
+        K1,
+        K2,
+        K3,
+    }
+}
